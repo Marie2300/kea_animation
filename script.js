4ff7c62ab3ed030c88eee633877cbe35255d1ed4
Marie2300 <marie_andersen@outlook.dk>
2019-01-31 16:46:42 +0100

    Mangler at fjerne hair

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ window.addEventListener("load", sidenVises);
 
 "use strict";
 let points = 0;
-let life = 4;
+let liv = "#life" + 3;
 
 function sidenVises() {
     console.log("All resources finished loading!");
+    showStart();
 }
-showStart();
+
 
 function showStart() {
     console.log("showStart");
     document.querySelector("#start").classList.remove("hide");
     document.querySelector("#start").classList.add("show");
     document.querySelector("#settings").addEventListener("click", showSettings);
+    document.querySelector("#settings_close").addEventListener("click", hideSettings);
     document.querySelector("#play").classList.add("pulse");
     document.querySelector("#play").addEventListener("click", hideStart);
 
@@ -27,6 +29,14 @@ function showSettings() {
     document.querySelector("settings_close").classList.add("pulse");
 }
 
+function hideSettings() {
+    console.log("hideSettings");
+    document.querySelector("#setting_screen").classList.add("hide");
+    document.querySelector("#settings").classList.add("show");
+
+}
+
+
 
 function hideStart() {
     console.log("hideStart");
@@ -38,6 +48,7 @@ function hideStart() {
 
 function startGame() {
     console.log("startGame");
+    document.querySelector("#settings").classList.add("hide")
     document.querySelector("#start").classList.add("hide");
     document.querySelector("#start").classList.remove("show");
 
@@ -77,74 +88,84 @@ function startGame() {
     document.querySelector("#vand16").classList.add("falling14");
     document.querySelector("#vand17").classList.add("falling15");
     document.querySelector("#vand18").classList.add("falling16");
-    document.querySelector("#vand").addEventListener("click", click);
-    document.querySelector("#vand3").addEventListener("click", click);
-    document.querySelector("#vand4").addEventListener("click", click);
-    document.querySelector("#vand5").addEventListener("click", click);
-    document.querySelector("#vand6").addEventListener("click", click);
-    document.querySelector("#vand7").addEventListener("click", click);
-    document.querySelector("#vand8").addEventListener("click", click);
-    document.querySelector("#vand9").addEventListener("click", click);
-    document.querySelector("#vand10").addEventListener("click", click);
-    document.querySelector("#vand11").addEventListener("click", click);
-    document.querySelector("#vand12").addEventListener("click", click);
-    document.querySelector("#vand13").addEventListener("click", click);
-    document.querySelector("#vand14").addEventListener("click", click);
-    document.querySelector("#vand15").addEventListener("click", click);
-    document.querySelector("#vand16").addEventListener("click", click);
-    document.querySelector("#vand17").addEventListener("click", click);
-    document.querySelector("#vand18").addEventListener("click", click);
-    document.querySelector("#snefnug1").addEventListener("click", click);
-    document.querySelector("#snefnug2").addEventListener("click", click);
-    document.querySelector("#snefnug3").addEventListener("click", click);
-    document.querySelector("#snefnug4").addEventListener("click", click);
-    document.querySelector("#snefnug5").addEventListener("click", click);
-    document.querySelector("#snefnug6").addEventListener("click", click);
-    document.querySelector("#snefnug7").addEventListener("click", click);
-    document.querySelector("#snefnug8").addEventListener("click", click);
-    document.querySelector("#snefnug9").addEventListener("click", click);
-    document.querySelector("#snefnug10").addEventListener("click", click);
-    document.querySelector("#snefnug11").addEventListener("click", click);
-    document.querySelector("#snefnug12").addEventListener("click", click);
-    document.querySelector("#snefnug13").addEventListener("click", click);
-    document.querySelector("#snefnug14").addEventListener("click", click);
-    document.querySelector("#snefnug15").addEventListener("click", click);
-    document.querySelector("#snefnug16").addEventListener("click", click);
 
+    document.querySelector("#vand").addEventListener("click", vandForsvind);
+    document.querySelector("#vand3").addEventListener("click", vandForsvind);
+    document.querySelector("#vand4").addEventListener("click", vandForsvind);
+    document.querySelector("#vand5").addEventListener("click", vandForsvind);
+    document.querySelector("#vand6").addEventListener("click", vandForsvind);
+    document.querySelector("#vand7").addEventListener("click", vandForsvind);
+    document.querySelector("#vand8").addEventListener("click", vandForsvind);
+    document.querySelector("#vand9").addEventListener("click", vandForsvind);
+    document.querySelector("#vand10").addEventListener("click", vandForsvind);
+    document.querySelector("#vand11").addEventListener("click", vandForsvind);
+    document.querySelector("#vand12").addEventListener("click", vandForsvind);
+    document.querySelector("#vand13").addEventListener("click", vandForsvind);
+    document.querySelector("#vand14").addEventListener("click", vandForsvind);
+    document.querySelector("#vand15").addEventListener("click", vandForsvind);
+    document.querySelector("#vand16").addEventListener("click", vandForsvind);
+    document.querySelector("#vand17").addEventListener("click", vandForsvind);
+    document.querySelector("#vand18").addEventListener("click", vandForsvind);
+
+    document.querySelector("#snefnug1").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug2").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug3").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug4").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug5").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug6").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug7").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug8").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug9").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug10").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug11").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug12").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug13").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug14").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug15").addEventListener("click", snefnugForsvind);
+    document.querySelector("#snefnug16").addEventListener("click", snefnugForsvind);
+}
+
+
+function vandForsvind() {
+    console.log("vandForsvind");
+    document.querySelector("#vand").classList.add("paused");
+    document.querySelector("#vand").classList.add("dissapear");
+
+    points++;
+    console.log(points);
+    document.querySelector("#points").innerHTML = "Points: " + points;
+
+    console.log(this);
+    this.classList.add("hide")
+}
+
+function snefnugForsvind() {
+    console.log("snefnugForsvind");
+
+    document.querySelector("#snefnug").classList.add("paused");
+    document.querySelector("#snefng").classList.add("dissapear");
+
+    life--;
+    document.querySelector("#energy").innerHTML = "liv: " + life;
+    console.log(life);
+
+
+    console.log(this);
+    this.classList.add("hide");
 
 }
 
-function click() {
-    console.log("click");
 
-    document.querySelector("click").classList.add("paused");
-    document.querySelector("click").classList.add("dissapear");
-
-    if (this.classList.contains("snefnug1")) {
-        console.log("snefnug1");
-        document.querySelector("#liv" + life).classList.add("hide");
-        --life;
-    } else if (this.classList.contains("vand1")) {
-        console.log("vand1");
-        points++;
-        document.querySelector("#points").innerHTML = points;
-
-    }
-
-    this.classList.add("dissapear");
-
-    gameStatus()
-
-}
+gameStatus()
 
 
 function gameStatus() {
     console.log("gameStatus");
+
     console.log(life);
     if (life == 0) {
         document.querySelector("#gameover").classList.remove("hide");
     } else if (points == 10) {
-
         document.querySelector("#levelop").classList.remove("hide");
     }
 
